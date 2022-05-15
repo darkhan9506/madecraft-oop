cdbfab31110f2695591995e7422d7678fe63722a
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-05-15 15:47:28 +0600

    Ocean class

--- 4. Static/exercises/Ocean.java
+++ 4. Static/exercises/Ocean.java
@@ -1,2 +1,22 @@
-package exercises;public class Ocean {
+package exercises;
+
+public class Ocean {
+
+  public static void main(String[] args) {
+    SeaCreature spongeBob = new SeaCreature("spongebob");
+    System.out.println(spongeBob.getName());
+    spongeBob.eat();
+    spongeBob.laugh();
+
+    SeaCreature patrick = new SeaCreature("patrick");
+    System.out.println(patrick.getName());
+    patrick.eat();
+    patrick.laugh();
+
+    SeaCreature squidward = new SeaCreature("squidward");
+    System.out.println(squidward.getName());
+    squidward.eat();
+    squidward.laugh();
+  }
+
 }
