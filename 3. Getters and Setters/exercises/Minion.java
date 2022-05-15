90392551b2822c7e5d3577f613e38a79cafddf9c
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-05-15 14:00:48 +0600

    getters and setters exercise

--- 3. Getters and Setters/exercises/Minion.java
+++ 3. Getters and Setters/exercises/Minion.java
@@ -2,4 +2,47 @@ package exercises;
 
 public class Minion {
 
+  private String name;
+  private int eyes;
+  private String color;
+  private String master;
+
+  public Minion(final String name, final int eyes, final String color, final String master) {
+    this.name = name;
+    this.eyes = eyes;
+    this.color = color;
+    this.master = master;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public int getEyes() {
+    return eyes;
+  }
+
+  public void setEyes(final int eyes) {
+    this.eyes = eyes;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(final String color) {
+    this.color = color;
+  }
+
+  public String getMaster() {
+    return master;
+  }
+
+  public void setMaster(final String master) {
+    this.master = master;
+  }
 }
